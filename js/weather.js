c9b08aeaac4f4bd13fe73b1d83da5d5886ee6881
Seth Brown <wsethbrown@gmail.com>
2021-02-02 12:59:35 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '25637923ec5e5f59dde1790913ab238a';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 33.9609; 
+  let longitude = -83.3779;
 
   getWeather(latitude, longitude);
 }
